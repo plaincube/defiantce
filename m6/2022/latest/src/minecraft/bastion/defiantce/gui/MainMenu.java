170d16065abb15927e20ee0250d589857784bd89
plaincube <notcubeboy@gmail.com>
2022-10-01 22:55:10 +0800

    FIXED: DCE 2022 m6 Sept final.

--- m6/2022/latest/src/minecraft/bastion/defiantce/gui/MainMenu.java
+++ m6/2022/latest/src/minecraft/bastion/defiantce/gui/MainMenu.java
@@ -43,12 +43,12 @@ public class MainMenu extends GuiScreen {
 		GlStateManager.translate(-(width / 2f), -(height / 2f), 0);
 		GlStateManager.popMatrix();
 		
-		this.drawCenteredString(mc.fontRendererObj, Defiant.name + Defiant.version + " by Cube and Healer", width /2f, height /2.5f - mc.fontRendererObj.FONT_HEIGHT /2f, -1);
+		this.drawCenteredString(mc.fontRendererObj, Defiant.name + " " + Defiant.version + " by Cube and Healer", width /2f, height /2.5f - mc.fontRendererObj.FONT_HEIGHT /2f, -1);
 		this.drawCenteredString(mc.fontRendererObj, "By Cube and Healer", width /2f, height /2.25f - mc.fontRendererObj.FONT_HEIGHT /2f, -1);
 		
 	}
 	
-	public void mousePressed(int mouseX, int mouseY, int button) {
+	public void mouseClicked(int mouseX, int mouseY, int button) {
 		
 		String[] buttons = {"SinglePlayer", "MultiPlayer", "Settings", "Language", "Quit"};
 		int count = 0;
@@ -61,19 +61,15 @@ public class MainMenu extends GuiScreen {
 				case "SinglePlayer":
 					mc.displayGuiScreen(new GuiSelectWorld(this));
 					break;
-					
 				case "MultiPlayer":
 					mc.displayGuiScreen(new GuiMultiplayer(this));
 					break;
-					
 				case "Settings":
 					mc.displayGuiScreen(new GuiOptions(this, mc.gameSettings));
 					break;
-					
 				case "Language":
 					mc.displayGuiScreen(new GuiLanguage(this, mc.gameSettings, mc.getLanguageManager()));
 					break;
-					
 				case "Quit":
 					mc.shutdown();
 					break;
