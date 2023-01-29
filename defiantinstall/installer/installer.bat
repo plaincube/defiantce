e7d5f3b622a74858edcee7f885fa37a3a920ff88
CubeUltm <93862196+plaincube@users.noreply.github.com>
2023-01-29 14:27:41 +0800

    Update installer.bat

--- defiantinstall/installer/installer.bat
+++ defiantinstall/installer/installer.bat
@@ -1,5 +1,23 @@
 @echo off
 title Defiant Installer
+
+:test
+cls
+echo. +===================+
+echo    Defiant Installer
+echo. +===================+
+echo Check directory settings. If incorrect, set from Installer settings (in menu).
+set /p installdir=<installdir.settings
+set /p mcdir=<mcdir.settings
+if (%installdir%) == () (echo Installer directory not set! Set from Installer settings (in menu))
+if (%mcdir%) == () (echo .minecraft directory not set! Set from Installer settings (in menu))
+echo Installation directory: %installdir%
+echo .minecraft directory: %mcdir%
+pause
+goto home
+
+:home
+cls
 wget -q https://raw.githubusercontent.com/plaincube/defiantce/main/defiantinstall/jars/jarlist.txt
 echo. +===================+
 echo    Defiant Installer
@@ -22,10 +40,48 @@ cls
 echo. +===================+
 echo    Defiant Installer
 echo. +===================+
-
+echo [1] Defiant Resource Pack
+echo [2] DefiantCE
+echo [3] Back
+echo.
+choice /c 1234 /n /m "Option (Numeral): "
+IF %ERRORLEVEL% EQU 1 goto dlres
+IF %ERRORLEVEL% EQU 2 goto dlclients
+IF %ERRORLEVEL% EQU 3 goto home
 
 :settings
 cls
+echo. +===================+
+echo    Defiant Installer
+echo. +===================+
+echo [1] Set Installer directory
+echo [2] Set .minecraft directory
+echo [3] Back
+echo.
+choice /c 1234 /n /m "Option (Numeral): "
+IF %ERRORLEVEL% EQU 1 goto setcd
+IF %ERRORLEVEL% EQU 2 goto setmc
+IF %ERRORLEVEL% EQU 3 goto home
+
+:setcd
+cls
+echo. +===================+
+echo    Defiant Installer
+echo. +===================+
+set /p %setcd%=Installation directory (without back "\"): 
+echo %setcd% > installdir.settings
+pause
+goto home
+
+:setmc
+cls
+echo. +===================+
+echo    Defiant Installer
+echo. +===================+
+set /p %setmc%=.minecraft directory (without back "\"): 
+echo %setmc% > mcdir.settings
+pause
+goto home
 
 :about
 cls
